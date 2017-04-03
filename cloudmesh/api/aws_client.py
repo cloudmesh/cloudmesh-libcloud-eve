a8efb74a53ee417f25d251e362bfddee70c583ab
MilindSuryawanshi <milind.sms@gmail.com>
2017-04-03 16:04:51 +0530

    added image list command with keys stored in yml file

--- cloudmesh/api/aws_client.py
+++ cloudmesh/api/aws_client.py
@@ -7,11 +7,17 @@ import os
 #import requests
 #import json
 import sys
-
+from libcloud.compute.types import Provider
+from libcloud.compute.providers import get_driver
+import yaml
 
 class Aws(object):
     def __init__(self):
         #self.client = docker.from_env()
+        f = open("cloudmesh/api/aws.yml");
+        self.Constants = yaml.safe_load(f)
+        f.close()
+        #print(self.Constants["user_details"]["ACCESS_ID"])
         print("TODO")
 
     def images_list(self):
@@ -21,8 +27,19 @@ class Aws(object):
         :rtype: NoneType
 
         """
-        print("List of images")
-        print("TODO")
+        cls = get_driver(Provider.EC2)
+        driver = cls(self.Constants["user_details"]["ACCESS_ID"], self.Constants["user_details"]["SECRET_KEY"], region="us-west-1")
 
+        sizes = driver.list_sizes()
+        images = driver.list_images()
+        print("List of images--------------")
+        print(images)
+        print("TODO")
+        #size = [s for s in sizes if s.id == 'performance1-1'][0]
+        #image = [i for i in images if 'Ubuntu 12.04' in i.name][0]
+
+        #node = driver.create_node(name='libcloud', size=size, image=image)
+        print("The Node is ---------------")
+        #print(node)
 
 
